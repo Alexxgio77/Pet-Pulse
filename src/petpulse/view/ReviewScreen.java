38d09fef173215697cd20d52bd5c8e5c3531603c
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 22:29:35 +0300

    Update ReviewScreen.java

--- src/petpulse/view/ReviewScreen.java
+++ src/petpulse/view/ReviewScreen.java
@@ -12,10 +12,10 @@ public class ReviewScreen {
     }
 
     public void displayWithExistingData(String comments, int rating) {
-        // Άνοιγμα φόρμας με συμπληρωμένα τα παλιά στοιχεία[cite: 55].
+        // Άνοιγμα φόρμας με συμπληρωμένα τα παλιά στοιχεία
     }
 
-    // Ο χρήστης βάζει τη βαθμολογία/σχόλια και πατάει «Υποβολή»[cite: 47, 56].
+    // Ο χρήστης βάζει τη βαθμολογία/σχόλια και πατάει «Υποβολή»
     public void clickSubmit(int rating, String comments) {
         controller.submitReview(rating, comments, appointment);
     }
