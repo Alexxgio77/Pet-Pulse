359f25207d5a283e4f739a0d1946b68eec08b884
giof5555 <giorgisf03@gmail.com>
2026-05-27 19:24:37 +0300

    Update DrugManager.java

--- src/petpulse/controller/DrugManager.java
+++ src/petpulse/controller/DrugManager.java
@@ -9,4 +9,21 @@ public class DrugManager {
         DrugScreen drugScreen = new DrugScreen();
         drugScreen.display(drugsData);
     }
+
+    private OrderManager orderManager = new OrderManager();
+
+    public void init() {
+        String drugsData = dbManager.fetchDrugs();
+        DrugScreen drugScreen = new DrugScreen(this);
+        drugScreen.display(drugsData); // [cite: 131]
+    }
+
+    public void makeOrder(String drugId, int quantity, DrugScreen screen) {
+        orderManager.sendOrder(drugId, quantity, screen);
+    }
+
+    public void cancel(DrugScreen screen) {
+        // Το σύστημα ακυρώνει τη διαδικασία και επιστρέφει τον χρήστη στην οθόνη των φαρμάκων [cite: 137]
+        System.out.println("Η διαδικασία ακυρώθηκε.");
+    }
 }
