7e289c2c0f424f13ee6ced8d84a2eeef44660db1
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 22:27:29 +0300

    Update HomeScreen.java

--- src/petpulse/view/HomeScreen.java
+++ src/petpulse/view/HomeScreen.java
@@ -1,7 +1,7 @@
 public class HomeScreen {
     private ManageAppointmentsClass controller = new ManageAppointmentsClass();
 
-    // Ο χρήστης επιλέγει από το αρχικό μενού την επιλογή «Αξιολόγηση Ραντεβού»[cite: 42].
+    // Ο χρήστης επιλέγει από το αρχικό μενού την επιλογή «Αξιολόγηση Ραντεβού»
     public void clickReviewAppointment() {
         controller.showMyAppointments();
     }
