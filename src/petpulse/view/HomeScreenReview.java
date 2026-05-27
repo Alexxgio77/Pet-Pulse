b6058d7026d699c0bfa80a17ea5d11875b883936
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 18:22:00 +0300

    Delete src/petpulse/view/HomeScreenReview.java

--- src/petpulse/view/HomeScreenReview.java
+++
@@ -1,8 +0,0 @@
-public class HomeScreen {
-    private ManageAppointmentsClass controller = new ManageAppointmentsClass();
-
-    // Ο χρήστης επιλέγει από το αρχικό μενού την επιλογή «Αξιολόγηση Ραντεβού»[cite: 42].
-    public void clickReviewAppointment() {
-        controller.showMyAppointments();
-    }
-}
