57d2b1f888a826a77828a74367744e6c59bac71a
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 22:34:42 +0300

    Update PetManager.java

--- src/petpulse/controller/PetManager.java
+++ src/petpulse/controller/PetManager.java
@@ -1,12 +1,12 @@
 public class PetManager {
     private DBManager dbManager = new DBManager();
 
-    // Το σύστημα ανακτά τα κατοικίδια που έχει εξετάσει ο κτηνίατρος [cite: 110]
+    // Το σύστημα ανακτά τα κατοικίδια που έχει εξετάσει ο κτηνίατρος [
     public void getExaminedPets() {
         String vetId = "vet_123";
         String petsData = dbManager.fetchPets(vetId);
         
-        // Δημιουργία και εμφάνιση της οθόνης επιλογής κατοικιδίου [cite: 110]
+        // Δημιουργία και εμφάνιση της οθόνης επιλογής κατοικιδίου 
         PetScreen petScreen = new PetScreen();
         petScreen.display(petsData);
     }
