4b69e32a83edb2fb6a1fedf6d7327c43446520b4
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 22:54:25 +0300

    Update HomeScreen.java

--- src/petpulse/view/HomeScreen.java
+++ src/petpulse/view/HomeScreen.java
@@ -4,5 +4,14 @@ public class HomeScreen {
     // Ο χρήστης επιλέγει από το αρχικό μενού την επιλογή «Αξιολόγηση Ραντεβού»
     public void clickReviewAppointment() {
         controller.showMyAppointments();
+    
+    
+    private ManagedDrugsClass drugsController = new ManagedDrugsClass();
+
+    // Ο χρήστης κάνει κλικ στην επιλογή «Διαχείριση Φαρμακείου» στην Αρχική Οθόνη [cite: 143]
+    public void clickPharmacyManagement() {
+        drugsController.init();
     }
 }
+
+
