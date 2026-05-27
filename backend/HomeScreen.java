ad1e29ff2fd09f308e7302b9ebf883f8144f8495
blatman13 <vlatanis@gmail.com>
2026-05-27 17:41:29 +0300

    Delete backend directory

--- backend/HomeScreen.java
+++
@@ -1,8 +0,0 @@
-public class HomeScreen {
-    private ProfileManager profileManager = new ProfileManager();
-
-    // Ο χρήστης επιλέγει την «Επεξεργασία Προφίλ» από το μενού [cite: 67, 74]
-    public void clickEditProfile() {
-        profileManager.retrieveProfileData(); [cite: 74]
-    }
-}
