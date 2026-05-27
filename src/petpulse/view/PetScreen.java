fd5376533e08acf474719708f1f0808dcc8cdae3
giof5555 <giorgisf03@gmail.com>
2026-05-27 18:17:53 +0300

    Add files via upload

---
+++ src/petpulse/view/PetScreen.java
@@ -0,0 +1,12 @@
+public class PetScreen {
+
+    public void display(String petsData) {
+        // Εμφάνιση της λίστας των εξετασμένων κατοικιδίων [cite: 110]
+    }
+
+    // Ο κτηνίατρος επιλέγει το κατοικίδιο που θέλει να συνταγογραφήσει [cite: 111]
+    public void selectPet(String petId) {
+        DrugManager drugManager = new DrugManager();
+        drugManager.getAvailableDrugs();
+    }
+}
