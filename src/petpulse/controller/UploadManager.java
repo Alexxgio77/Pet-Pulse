a513a5347f38b800c599d05cc755230433c52d9b
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 22:36:34 +0300

    Update UploadManager.java

--- src/petpulse/controller/UploadManager.java
+++ src/petpulse/controller/UploadManager.java
@@ -1,26 +1,26 @@
 public class UploadManager {
     private DBManager dbManager = new DBManager();
 
-    // Προετοιμασία επιβεβαίωσης [cite: 115]
+    // Προετοιμασία επιβεβαίωσης 
     public void prepConf() {
         ConfScreen confScreen = new ConfScreen(this);
         confScreen.display();
     }
 
-    // Ο χρήστης επιβεβαιώνει την έκδοση [cite: 116]
+    // Ο χρήστης επιβεβαιώνει την έκδοση 
     public void confirm() {
         dbManager.submitPrescription();
-        dbManager.saveAndSend(); // Αποστολή στο προφίλ του ιδιοκτήτη [cite: 117]
+        dbManager.saveAndSend(); // Αποστολή στο προφίλ του ιδιοκτήτη 
         System.out.println("statusOK");
     }
 
-    // Εναλλακτική Ροή 2: Ο χρήστης πατάει «Ακύρωση» [cite: 124]
+    // Εναλλακτική Ροή 2: Ο χρήστης πατάει «Ακύρωση» 
     public void cancel() {
         DrugScreen drugScreen = new DrugScreen();
-        drugScreen.display("refresh_drugs"); // Επιστροφή στη λίστα φαρμάκων [cite: 125]
+        drugScreen.display("refresh_drugs"); // Επιστροφή στη λίστα φαρμάκων 
     }
 
-    // Μετάβαση στο Use Case «Αίτηση Παραγγελίας Φαρμάκου» [cite: 121]
+    // Μετάβαση στο Use Case «Αίτηση Παραγγελίας Φαρμάκου» 
     public void initiateOrderReq() {
         System.out.println("Εκτέλεση UC: Αίτηση Παραγγελίας Φαρμάκου");
     }
