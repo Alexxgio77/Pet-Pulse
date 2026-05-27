923e1fc6e93677f7d0f4de2caea786e43c79ada4
Alexxgio77 <alexxgio77@gmail.com>
2026-05-27 22:33:57 +0300

    Remove citations from comments in ManageReviewScreen

--- src/petpulse/controller/ManageReviewScreen.java
+++ src/petpulse/controller/ManageReviewScreen.java
@@ -1,16 +1,16 @@
 public class ManageReviewScreen {
     
-    // Το σύστημα ελέγχει αν υπάρχει ήδη αξιολόγηση για το ραντεβού[cite: 45].
+    // Το σύστημα ελέγχει αν υπάρχει ήδη αξιολόγηση για το ραντεβού
     public void checkReviewExistence(Appointment appointment) {
         boolean isReviewed = false; // Ερώτημα στο ReviewDB
         
         if (isReviewed) {
-            // Εναλλακτική Ροή 1: Το σύστημα βρίσκει ήδη καταχωρημένη αξιολόγηση[cite: 52].
-            // Εμφανίζει μήνυμα και την επιλογή «Επεξεργασία αξιολόγησης»[cite: 53].
+            // Εναλλακτική Ροή 1: Το σύστημα βρίσκει ήδη καταχωρημένη αξιολόγηση
+            // Εμφανίζει μήνυμα και την επιλογή «Επεξεργασία αξιολόγησης»
             ExistingReviewsScreen existingScreen = new ExistingReviewsScreen(this, appointment);
             existingScreen.display();
         } else {
-            // Βασική Ροή: Το σύστημα βλέπει πως δεν υπάρχει ήδη αξιολόγηση[cite: 46].
+            // Βασική Ροή: Το σύστημα βλέπει πως δεν υπάρχει ήδη αξιολόγηση
             ReviewScreen reviewScreen = new ReviewScreen(this, appointment);
             reviewScreen.display();
         }
@@ -18,23 +18,23 @@ public class ManageReviewScreen {
 
     // Έλεγχος και αποθήκευση της αξιολόγησης
     public void submitReview(int rating, String comments, Appointment appointment) {
-        // Το σύστημα ελέγχει αν η αξιολόγηση είναι έγκυρη[cite: 48].
+        // Το σύστημα ελέγχει αν η αξιολόγηση είναι έγκυρη
         boolean isValid = rating >= 1 && rating <= 5; 
 
         if (!isValid) {
-            // Εναλλακτική Ροή 2: Το σύστημα κρίνει την αξιολόγηση μη έγκυρη[cite: 59].
-            // Εμφανίζει μήνυμα σφάλματος και επιστρέφει στη φόρμα[cite: 60].
+            // Εναλλακτική Ροή 2: Το σύστημα κρίνει την αξιολόγηση μη έγκυρη
+            // Εμφανίζει μήνυμα σφάλματος και επιστρέφει στη φόρμα
             ErrorScreen errorScreen = new ErrorScreen();
             errorScreen.display();
             
             ReviewScreen reviewScreen = new ReviewScreen(this, appointment);
             reviewScreen.display();
         } else {
-            // Το σύστημα αποθηκεύει την αξιολόγηση στο ιστορικό και στο προφίλ του κτηνιάτρου[cite: 49].
+            // Το σύστημα αποθηκεύει την αξιολόγηση στο ιστορικό και στο προφίλ του κτηνιάτρου
             Review newReview = new Review(rating, comments, appointment);
             // Ενημέρωση στα ReviewDB και VetProfileDB
             
-            // Εμφανίζει μήνυμα επιβεβαίωσης και επιστρέφει στη λίστα[cite: 50].
+            // Εμφανίζει μήνυμα επιβεβαίωσης και επιστρέφει στη λίστα
             System.out.println("Η αξιολόγηση καταχωρήθηκε επιτυχώς");
             ManageAppointmentsClass appointmentsController = new ManageAppointmentsClass();
             appointmentsController.showMyAppointments();
