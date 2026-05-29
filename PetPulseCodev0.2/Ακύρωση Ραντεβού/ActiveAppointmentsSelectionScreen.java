1a30b76b2f7bf094266bf0c32ea728b837cde43f
Alexxgio77 <alexxgio77@gmail.com>
2026-05-29 17:54:01 +0300

    Add files via upload

--- PetPulseCodev0.2/Ακύρωση Ραντεβού/ActiveAppointmentsSelectionScreen.java
+++ PetPulseCodev0.2/Ακύρωση Ραντεβού/ActiveAppointmentsSelectionScreen.java
@@ -3,18 +3,32 @@ import java.util.Scanner;
 
 public class ActiveAppointmentsSelectionScreen {
     private ManageAppointmentsClass controller;
-    public ActiveAppointmentsSelectionScreen(ManageAppointmentsClass c) { this.controller = c; }
+
+    public ActiveAppointmentsSelectionScreen(ManageAppointmentsClass c) { 
+        this.controller = c; 
+    }
 
     public void display(List<Appointment> apps) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n--- ΟΘΟΝΗ ΕΝΕΡΓΩΝ ΡΑΝΤΕΒΟΥ (UC 2) ---");
-        if(apps.isEmpty()) { new HomeScreen().display(); return; }
-        for (int i = 0; i < apps.size(); i++) {
-            System.out.println((i + 1) + ". " + apps.get(i).getDateTime() + " [" + apps.get(i).getStatus() + "]");
+        System.out.println("\n--- ΟΘΟΝΗ ΕΝΕΡΓΩΝ ΡΑΝΤΕΒΟΥ ---");
+        
+        if(apps.isEmpty()) { 
+            System.out.println("Δεν υπάρχουν εκκρεμή ραντεβού."); 
+            new HomeScreen().display(); 
+            return; 
         }
+        
+        for (int i = 0; i < apps.size(); i++) {
+            System.out.println((i + 1) + ". " + apps.get(i).getDateTime() + " | Ζώο: " + apps.get(i).getPet().getName());
+        }
+        
         System.out.print("Επιλέξτε ραντεβού για ακύρωση: ");
         int choice = scanner.nextInt() - 1;
-        if (choice >= 0 && choice < apps.size()) controller.returnSelection(apps.get(choice));
-        else new HomeScreen().display();
+        
+        if (choice >= 0 && choice < apps.size()) {
+            controller.returnSelection(apps.get(choice));
+        } else {
+            new HomeScreen().display();
+        }
     }
 }
