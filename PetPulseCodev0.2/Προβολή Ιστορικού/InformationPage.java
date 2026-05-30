9195cfa105b2f2a79d534b4a0434f2ca1fc0ee23
Nikos Chatzakis <107366687+NikosChatzakis@users.noreply.github.com>
2026-05-30 17:15:40 +0300

    Add files via upload

---
+++ PetPulseCodev0.2/Προβολή Ιστορικού/InformationPage.java
@@ -0,0 +1,7 @@
+public class InformationPage {
+    public void display(String data) {
+        System.out.println("\n====================================");
+        System.out.println(data);
+        System.out.println("====================================");
+    }
+}
