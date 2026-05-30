2d381db1d792ed0ec05c12d44df427f6996e5764
Nikos Chatzakis <107366687+NikosChatzakis@users.noreply.github.com>
2026-05-30 17:14:42 +0300

    Delete PetPulseCodev0.2/Προγραμματισμός Ραντεβού/AppointmentsManager.java

--- PetPulseCodev0.2/Προγραμματισμός Ραντεβού/AppointmentsManager.java
+++
@@ -1,5 +0,0 @@
-public class AppointmentsManager {
-    public void getAppointments(String petName) {
-        new InformationPage().display("Ιατρικό Ιστορικό για τον " + petName + ":\n- 10/01/2026: Αντιλυσσικό Εμβόλιο");
-    }
-}
