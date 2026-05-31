4ac9a256139198ee16c7d760325c245f96e1a872
Alexxgio77 <alexxgio77@gmail.com>
2026-05-31 19:00:34 +0300

    Add files via upload

--- PetPulseCodev0.2/Διαχείριση Φαρμακείου/ManageOrderClass.java
+++ PetPulseCodev0.2/Διαχείριση Φαρμακείου/ManageOrderClass.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
 public class ManageOrderClass {
-    public void init() {
+    private DBManager dbManager = new DBManager();
+
+    public void init(String drugName) {
+        System.out.println("\n--- ΑΝΟΙΓΜΑ ΦΟΡΜΑΣ ΠΑΡΑΓΓΕΛΙΑΣ ΦΑΡΜΑΚΩΝ ---");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Εισάγετε ποσότητα τεμαχίων για παραγγελία: ");
+        System.out.print("Εισάγετε ποσότητα τεμαχίων για ανεφοδιασμό: ");
         int qty = scanner.nextInt();
-        new DBManager().addOrderedItems(qty);
-        new PharmacyOrderConfirmationScreen().display();
+        
+        if (qty <= 0) {
+            System.out.println("\n❌ Σφάλμα: Η ποσότητα παραγγελίας πρέπει να είναι μεγαλύτερη από 0.");
+            return;
+        }
+        
+        order(drugName, qty);
+    }
+
+    public void order(String drugName, int quantity) {
+        dbManager.addOrderedItems(quantity);
+        dbManager.restockDrug(drugName, quantity);
+        PharmacyOrderConfirmationScreen confScreen = new PharmacyOrderConfirmationScreen();
+        confScreen.display();
     }
 }
